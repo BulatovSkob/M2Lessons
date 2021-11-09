2d9fd9de83a7e40dcca6762c5410bba9040b3979
viacheslav.bulatov viacheslav.bulatov@vaimo.com <viacheslav.bulatov@vaimo.com>
2021-11-09 13:07:04 +0100

    graphql ready

---
+++ app/code/Bulatov/BlogGraphQL/GraphQl/Resolver/AllBlogs.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Bulatov\BlogGraphQL\GraphQl\Resolver;
+
+use Bulatov\BlogCore\Api\BlogRepositoryInterface;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class AllBlogs implements ResolverInterface
+{
+
+    private BlogRepositoryInterface $blogRepository;
+
+    public function __construct(BlogRepositoryInterface $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        return ['items' => $this->blogRepository->getAll()];
+    }
+}
